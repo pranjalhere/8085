8a0bff39794eab9000546d70e0e861449166797d
Pranjal Choudhary <pranjal20aug@gmail.com>
2023-05-29 20:29:21 +0530

    jc half done

--- Engine/nextAddress.cpp
+++ Engine/nextAddress.cpp
@@ -8,17 +8,18 @@ string nextAddress(string pc,int operation){
         if(pc[i]>='0' && pc[i]<='9'){
             int k = (pc[i]-'0')+arr[i];
             if(k>=0 && k<=9){
-                continue;
-            }else{
+                pc[i]='0'+ k;
+            }
+            else{
                 pc[i]=((k-10)+'A');
             }
 
-        }else if(pc[i]>='A' && pc[i]<='F'){
+        }
+        else if(pc[i]>='A' && pc[i]<='F'){
             int k = pc[i]-'A'+10;
             k+=operation;
-            pc[i]=k%10;
-            k/=10;
-            pc[i-1]=k;
+            k=k%10;
+            pc[i]='A'+k;
         }
     }
     return pc;
